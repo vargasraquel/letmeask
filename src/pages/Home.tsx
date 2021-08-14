6a89cdb6fbd06440633ec170ce094c237ef37271
Raquel Vargas <58276070+raquelvargasdev@users.noreply.github.com>
2021-08-14 14:05:34 -0300

    Adicionando Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,36 @@
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg'
+
 export function Home() {
     return (
         <div>
             <aside>
-                
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as dúvidas de sua audiência em tempo real</p>
             </aside>
+            <main>
+                <div>
+                    <img src={logoImg} alt="Letmeask" />
+                    <button>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>
+                        ou entre em sua sala
+                    </div>
+                    <form>
+                        <input 
+                            type="text"
+                            placeholder="Digite o código da sala" 
+                        />
+                        <button type="submit">
+                            Entrar na sala
+                        </button>
+                    </form>
+                </div>
+            </main>
         </div>
     )
 }
