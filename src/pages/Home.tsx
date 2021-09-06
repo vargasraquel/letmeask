bb770f5b1d17c2791adecfb4d0f7df67ffddbc77
Raquel Vargas <58276070+vargasraquel@users.noreply.github.com>
2021-09-06 19:25:22 -0300

    Concluindo a aplicação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,6 +41,11 @@ export function Home() {
             return;
         }
 
+        if (roomRef.val().endedAt) {
+            alert('Room has ended.')
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`)
     }
 
