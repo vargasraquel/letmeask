af1c8fad79dbbb0cef2df946642c145d762edf54
Raquel Vargas <58276070+vargasraquel@users.noreply.github.com>
2021-09-07 13:19:47 -0300

    Finalizando aplicação e rodando no firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ import { database } from '../services/firebase';
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
-    const [roomCode, setRoomCode] = useState('8');
+    const [roomCode, setRoomCode] = useState('');
 
     async function handleCreateRoom() {
         if(!user) {
